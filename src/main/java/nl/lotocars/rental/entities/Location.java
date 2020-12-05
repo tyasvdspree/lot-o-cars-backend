e94df95929ef3e15b9b7575c677bc55f7c66dc97
Mike van Niel <0152110@hr.nl>
2020-12-05 13:48:37 +0100

    Location changes lat/long

--- src/main/java/nl/lotocars/rental/entities/Location.java
+++ src/main/java/nl/lotocars/rental/entities/Location.java
@@ -3,6 +3,7 @@ package nl.lotocars.rental.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -11,8 +12,8 @@ import javax.persistence.Table;
 @Entity(name = "location")
 @Table(name = "LOCATION")
 public class Location extends BaseEntity{
-    private String Long;
-    private String lat;
+    private String longitude;
+    private String latitude;
     private String addressLine1;
     private String addressLine2;
     private String city;
---
+++ target/generated-sources/annotations/nl/lotocars/rental/mapper/CarMapperImpl.java
@@ -0,0 +1,75 @@
+package nl.lotocars.rental.mapper;
+
+import javax.annotation.processing.Generated;
+import nl.lotocars.rental.dtos.CarDto;
+import nl.lotocars.rental.entities.Car;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-12-04T23:53:42+0100",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (AdoptOpenJDK)"
+)
+@Component
+public class CarMapperImpl extends CarMapper {
+
+    @Override
+    public CarDto mapToDestination(Car source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        CarDto carDto = new CarDto();
+
+        carDto.setUser( source.getUser() );
+        carDto.setLocation( source.getLocation() );
+        carDto.setNumberPlate( source.getNumberPlate() );
+        carDto.setCountryCode( source.getCountryCode() );
+        carDto.setModelYear( source.getModelYear() );
+        carDto.setMake( source.getMake() );
+        carDto.setModel( source.getModel() );
+        carDto.setBody( source.getBody() );
+        carDto.setTransmission( source.getTransmission() );
+        carDto.setFuel( source.getFuel() );
+        carDto.setFuelUsage( source.getFuelUsage() );
+        carDto.setDoors( source.getDoors() );
+        carDto.setSeats( source.getSeats() );
+        carDto.setBootSpaceInLiters( source.getBootSpaceInLiters() );
+        carDto.setNavigation( source.isNavigation() );
+        carDto.setAirco( source.isAirco() );
+        carDto.setSmokingIsAllowed( source.isSmokingIsAllowed() );
+        carDto.setActive( source.isActive() );
+
+        return carDto;
+    }
+
+    @Override
+    public Car mapToSource(CarDto destination) {
+        if ( destination == null ) {
+            return null;
+        }
+
+        Car car = new Car();
+
+        car.setUser( destination.getUser() );
+        car.setLocation( destination.getLocation() );
+        car.setNumberPlate( destination.getNumberPlate() );
+        car.setCountryCode( destination.getCountryCode() );
+        car.setModelYear( destination.getModelYear() );
+        car.setMake( destination.getMake() );
+        car.setModel( destination.getModel() );
+        car.setBody( destination.getBody() );
+        car.setTransmission( destination.getTransmission() );
+        car.setFuel( destination.getFuel() );
+        car.setFuelUsage( destination.getFuelUsage() );
+        car.setDoors( destination.getDoors() );
+        car.setSeats( destination.getSeats() );
+        car.setBootSpaceInLiters( destination.getBootSpaceInLiters() );
+        car.setNavigation( destination.isNavigation() );
+        car.setAirco( destination.isAirco() );
+        car.setSmokingIsAllowed( destination.isSmokingIsAllowed() );
+        car.setActive( destination.isActive() );
+
+        return car;
+    }
+}
