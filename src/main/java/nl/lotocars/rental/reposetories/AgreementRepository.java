85956574374bd21e8995ad6a8d2beadb07133617
MikeVanNiel <38783806+MikeVanNiel@users.noreply.github.com>
2020-12-14 11:38:21 +0100

    Added agreements controller for getting rented dates (#6)
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>

---
+++ src/main/java/nl/lotocars/rental/reposetories/AgreementRepository.java
@@ -0,0 +1,25 @@
+package nl.lotocars.rental.reposetories;
+
+import nl.lotocars.rental.entities.Agreement;
+import nl.lotocars.rental.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Repository
+public interface AgreementRepository extends JpaRepository<Agreement, Long> {
+
+    @Query("SELECT a FROM Agreement a WHERE " +
+            "(:numberPlate is null OR a.car.numberPlate = :numberPlate) AND " +
+            "(a.startDate >= :fromDate)"
+    )
+    Collection<Agreement> findBySearchOptions(
+            @Param("numberPlate") String numberPlate,
+            @Param("fromDate") Date fromDate
+    );
+
+}
