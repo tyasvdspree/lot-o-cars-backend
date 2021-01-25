47929e34b0907bdab256b51c15856898af988327
tyasvdspree <tyasvanderspree@live.nl>
2021-01-25 13:49:31 +0100

    fixed some issues with tests

--- src/test/java/nl/lotocars/rental/services/CarServiceTest.java
+++ src/test/java/nl/lotocars/rental/services/CarServiceTest.java
@@ -1,5 +1,6 @@
 package nl.lotocars.rental.services;
 
+import nl.lotocars.rental.RentalApplication;
 import nl.lotocars.rental.entities.Car;
 import nl.lotocars.rental.entities.User;
 import nl.lotocars.rental.reposetories.CarRepository;
@@ -10,9 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,7 +22,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ContextConfiguration(classes = { CarService.class, CarRepository.class, UserRepository.class}, loader = AnnotationConfigContextLoader.class)
+@DataJpaTest
+@ContextConfiguration(classes = {RentalApplication.class, CarService.class, CarRepository.class, UserRepository.class})
 public class CarServiceTest {
     @Mock
     CarRepository carRepository;
