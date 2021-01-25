9f48ed5c217d078d2bd822ed9cf33a319e0edd71
jordy0934433 <34245247+jordy0934433@users.noreply.github.com>
2021-01-25 14:40:00 +0100

    Dev in master (#43)
    * travis config (#1)
    
    * Develop master (#11)
    
    * initial commit
    
    * first stable version
    
    * Changed longitude/latitude and capitals
    
    * get cars from backend
    
    * Added color property to Car
    
    * Location changes lat/long (#3)
    
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>
    
    * WIP: search cars on multiple criteria
    
    * WIP: search cars on search criteria
    
    * works
    
    * renaming
    
    * Solved searching on boolean values
    
    * Solved searching on modelyear
    
    * car register backend
    
    * merge
    
    * fix gitnore and stoped tracking target files
    
    * stabbel build
    
    * fix cors issues. add cors headers to each controller and cors config (#4)
    
    * Register user2 (#5)
    
    * add register user service, and refactor a few names
    
    * added bcrypt
    
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>
    
    * corsed issue resolved (#7)
    
    * corsed issue resolved
    
    * jwtExpirationInMillis
    
    * Merge branch 'develop' of https://github.com/tyasvdspree/lot-o-cars-backend into login
    
    # Conflicts:
    #	src/main/java/nl/lotocars/rental/configs/CorsConfig.java
    
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>
    
    * Added agreements controller for getting rented dates (#6)
    
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>
    
    * jwt key fix
    
    * jwt key fix (#8)
    
    * fix double deffinition
    
    * no authentication in backend (#10)
    
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>
    Co-authored-by: Mike van Niel <0152110@hr.nl>
    Co-authored-by: MikeVanNiel <38783806+MikeVanNiel@users.noreply.github.com>
    Co-authored-by: Jordy <34245247+jordy0934433@users.noreply.github.com>
    Co-authored-by: Jordy <0934433@hr.nl>
    
    * duplicate userdetailsservice implementation
    
    * jdk 11
    
    Co-authored-by: tyasvdspree <tyasvanderspree@live.nl>
    Co-authored-by: VolkanBozdogan <34245274+VolkanBozdogan@users.noreply.github.com>
    Co-authored-by: Mike van Niel <0152110@hr.nl>
    Co-authored-by: MikeVanNiel <38783806+MikeVanNiel@users.noreply.github.com>

---
+++ src/main/java/nl/lotocars/rental/Errors/UserNotFoundException.java
@@ -0,0 +1,22 @@
+package nl.lotocars.rental.Errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+    private static final String message = "user not Found";
+
+    public UserNotFoundException() {
+        super(message);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
