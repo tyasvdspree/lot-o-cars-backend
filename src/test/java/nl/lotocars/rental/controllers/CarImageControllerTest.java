d9465375961a76101182fc8108ec28aff49800a5
Volkan <volkanbozdogan84@gmail.com>
2021-01-25 13:26:22 +0100

    setup tests

--- src/test/java/nl/lotocars/rental/controllers/CarImageControllerTest.java
+++ src/test/java/nl/lotocars/rental/controllers/CarImageControllerTest.java
@@ -1,18 +1,33 @@
 package nl.lotocars.rental.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.lotocars.rental.configs.WebSecurityConfig;
+import nl.lotocars.rental.reposetories.CarImageRepository;
+import nl.lotocars.rental.reposetories.UserRepository;
+import nl.lotocars.rental.security.JwtAuthenticationFilter;
+import nl.lotocars.rental.security.JwtProvider;
+import nl.lotocars.rental.services.CarImageService;
+import nl.lotocars.rental.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = UserController.class)
+@ContextConfiguration
 public class CarImageControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -20,8 +35,17 @@ public class CarImageControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @InjectMocks
+    CarImageController carImageController;
+
+    @Mock
+    CarImageService carImageService;
+
+
     @Test
-    void getImageIds() {
+    void getImageIds() throws Exception {
+        String numberPlate = "H-735-GT";
+        MvcResult res = mockMvc.perform(get("/"+numberPlate)).andReturn();
     }
 
     @Test
--- src/test/java/nl/lotocars/rental/controllers/UserControllerTest.java
+++ src/test/java/nl/lotocars/rental/controllers/UserControllerTest.java
@@ -1,15 +1,27 @@
 package nl.lotocars.rental.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.lotocars.rental.entities.User;
+import nl.lotocars.rental.reposetories.CarRepository;
+import nl.lotocars.rental.reposetories.UserRepository;
+import nl.lotocars.rental.security.JwtProvider;
+import nl.lotocars.rental.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = UserController.class)
@@ -20,9 +32,17 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private UserService userService;
+
+    @Mock
+    JwtProvider jwtProvider;
+
     @Test
     void getUsers() {
-
     }
 
     @Test
